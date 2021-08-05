58f553de03c59b46c457fd6a063baa364371887d
ChloeLL <chloell@protonmail.ch>
2021-08-05 14:02:18 -0700

    Add offset creation functionality

--- modules/dateable.rb
+++ modules/dateable.rb
@@ -1,19 +1,23 @@
 module Dateable
-  @date = Time.now.strftime(“%d%m%y”)
-
-  def get_key_a
-    @date[0,2]
+  def create_offset
+    date = Time.now.strftime('%d%m%y').to_i
+    square = date ** 2
+    @offset = offset = square.to_s[-4..-1].to_i
   end
 
-  def get_key_b
-    @date[1,2]
+  def get_offset_a
+    @offset[0,2]
   end
 
-  def get_key_c
-    @date[2,2]
+  def get_offset_b
+    @offset[1,2]
   end
 
-  def get_key_d
-    @date[3,2]
+  def get_offset_c
+    @offset[2,2]
+  end
+
+  def get_offset_d
+    @offset[3,2]
   end
 end
