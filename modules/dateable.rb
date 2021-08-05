109f577ab47574b0b1b243dcfb7017c917bb2d91
ChloeLL <chloell@protonmail.ch>
2021-08-05 14:51:27 -0700

    Finalize offset creation

--- modules/dateable.rb
+++ modules/dateable.rb
@@ -1,23 +1,11 @@
 module Dateable
-  def create_offset
-    date = Time.now.strftime('%d%m%y').to_i
-    square = date ** 2
-    @offset = offset = square.to_s[-4..-1].to_i
+  def create_offsets(date = nil)
+    date    = Time.now.strftime('%d%m%y').to_i if date.nil?
+    square  = date ** 2
+    @offset = square.to_s[-4..-1]
   end
 
-  def get_offset_a
-    @offset[0,2]
-  end
-
-  def get_offset_b
-    @offset[1,2]
-  end
-
-  def get_offset_c
-    @offset[2,2]
-  end
-
-  def get_offset_d
-    @offset[3,2]
+  def get_offset(shift)
+    @offset[shift]
   end
 end
