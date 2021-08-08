aba7e70faa05654d947ab18171a70703db45343e
ChloeLL <chloell@protonmail.ch>
2021-08-07 21:17:12 -0700

    Move offset and key functionality to shift.rb

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,7 @@
-require './modules/offsetable'
-require './modules/keyable'
-
 class Enigma
-  include Offsetable
-  include Keyable
-
   def encrypt(message, key = nil, date = nil)
-    create_offsets(date)
-    create_keys(key)
   end
 
   def decrypt(message, key = nil, date = nil)
-    create_offsets(date)
-    create_keys(key)
   end
 end
