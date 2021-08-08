0baa1765e1fb360fe3e3fc2b89399e788d9cba66
ChloeLL <chloell@protonmail.ch>
2021-08-07 21:48:28 -0700

    Create shift object in Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,9 @@
+require './shift'
+
 class Enigma
   def encrypt(message, key = nil, date = nil)
+    shift = Shift.new
+    shift.create_shifts(key, date)
   end
 
   def decrypt(message, key = nil, date = nil)
